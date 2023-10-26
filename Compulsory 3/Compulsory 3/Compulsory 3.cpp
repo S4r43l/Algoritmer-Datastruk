3a8d4447c3a62c9bd32f11b703295a1cffebe955
Sara Emilie Lauvstad <selauvstad@gmail.com>
2023-10-26 03:46:47 +0200

    added more to trees

--- Compulsory 3/Compulsory 3/Compulsory 3.cpp
+++ Compulsory 3/Compulsory 3/Compulsory 3.cpp
@@ -7,20 +7,18 @@
 using namespace std;
 
 		/*  TREES  */
-class Node
+class TreeNode
 {
 public:
-	int data;
-	Node* parent;
-	vector<Node*>child;
-
-
+    int data;
+	TreeNode* parent;
+	vector<TreeNode*>child;
 };
 
-
-Node* CreateTreeNode(int data, Node* parent)
+//Insertion
+TreeNode* CreateTreeNode(int data, TreeNode* parent)
 {
-    Node* newNode = new Node();
+    TreeNode* newNode = new TreeNode();
 
     newNode->data = data;
     newNode->parent = parent;
@@ -33,107 +31,226 @@ Node* CreateTreeNode(int data, Node* parent)
     return newNode;
 }
 
-Node* GetParent(Node* tree)
+//Deletion (deletes node + all children and their decendants
+void deleteNode(TreeNode* tree)
 {
-    if (tree !=nullptr)
+        // Delete the children and their descendants first
+    while (!tree->child.empty())
     {
-        return tree->parent;
+        deleteNode(tree->child.back());
     }
-    
-}
 
-void isRoot(Node* tree)
-{
-    if (tree->parent != nullptr)
+    if (tree != nullptr && tree->parent != nullptr)
     {
-        cout << "no its not root!";
-    }
+        //removes the node
+		tree->parent->child.erase(remove(tree->parent->child.begin(),tree->parent->child.end(), tree), tree->parent->child.end());
+        //deletes the node
+        delete tree;
+
+        
+	}
     else
     {
-        cout << "yes its root";
-    }
+		cout << "Can't delete the root" << endl << endl;
+	}
+	cout << endl;
 }
 
 
-Node* GetRoot(Node* tree)
+//Access functions
+TreeNode* GetRoot(TreeNode* tree)
 {
-	while(tree->parent != nullptr)
-	{
+    while (tree->parent != nullptr)
+    {
         tree = tree->parent;
-	}
+    }
+
+    cout  << tree->data << ",is root" << endl;
+    cout << endl;
+
     return tree;
 }
 
-Node* GetChildren(Node* tree)
+TreeNode* GetParent(TreeNode* tree)
 {
-    //?
-    return 0;
+    if (tree->parent != nullptr)
+    {
+        tree = tree->parent;
+        cout << tree->data << " is the parent" << endl << endl;
+        return tree->parent;
+    }
+    else
+    {
+		cout << "Doesn't have a parent" << endl << endl;
+        return nullptr;
+	}
+    cout << endl;
 }
 
-void isLeaf(Node* tree)
+void GetChildren(TreeNode* tree)
 {
-    //?
+   
+    if (tree->child.empty())
+    {
+        cout << "It has no children" << endl;
+       
+    }
+
+    else
+    {
+        cout << "Children: ";
+        for (TreeNode* child : tree->child)
+        {
+            cout << child->data << " ";
+        }
+        cout << endl;
+    }
+    cout << endl;
+    
 }
 
-
-void isEmpety(Node* tree)
+//Query Functions
+void Size(TreeNode* tree)
 {
-	//?
+    int size = 0;
+    stack<TreeNode*> Stack;
+
+    Stack.push(tree);
+
+    while (!Stack.empty()) 
+    {
+		TreeNode* temp = Stack.top();
+		Stack.pop();
+
+		size++;
+
+        for (int i = temp->child.size() - 1; i >= 0; i--)
+        {
+			Stack.push(temp->child[i]);
+		}
+	}
+    cout << "Size of tree: " << size << endl << endl;
+
 }
 
-void preorderTraversal(Node* root)
+void Depth(TreeNode* tree)
 {
-    stack<Node*> Stack;
+	int depth = 1;
+    while (tree->parent != nullptr)
+    {
+		tree = tree->parent;
+		depth++;
+	}
+    	cout << "Depth of tree: " << depth << endl << endl;
+}
 
-    // 'Preorder'-> contains all the
-    // visited nodes
-    vector<int> Preorder;
+void isEmpety(TreeNode* tree)
+{
+    if (tree->child.empty())
+    {
+        cout << "it is empty" << endl << endl;
+    }
+    else
+    {
+        cout << "it is not empty" << endl << endl;
+    }
+}
+
+void isRoot(TreeNode* tree)
+{
+    if (tree->parent != nullptr)
+    {
+        cout << "no, its not root!" << endl << endl;
+    }
+    else
+    {
+        cout << "yes, its root" << endl <<endl;
+    }
+}
+
+void isLeaf(TreeNode* tree)
+{
+    if (tree->child.size() == 0)
+    {
+		cout << "yes, its leaf" << endl << endl;
+	}
+    else
+    {
+		cout << "no, its not leaf" << endl << endl;
+	}
+
+}
+
+//Traversal
+void preorderTraversal(TreeNode* root)
+{
+    //DLR - data, left, right (preorder)
+    stack<TreeNode*> Stack;
 
     Stack.push(root);
 
     while (!Stack.empty()) {
-        Node* temp = Stack.top();
+
+        TreeNode* temp = Stack.top();
         Stack.pop();
-        // store the key in preorder vector(visited list)
-        Preorder.push_back(temp->data);
-        // Push all of the child nodes of temp into
-        // the stack from right to left.
-        for (int i = temp->child.size() - 1; i >= 0; i--) {
+
+        cout << temp->data << " ";
+
+        for (int i = temp->child.size() - 1; i >= 0; i--) 
+        {
             Stack.push(temp->child[i]);
         }
-    }
-    for (auto i : Preorder) {
-        cout << i << " ";
-    }
-    cout << endl;
 
-    //DOEST WORK WHY THO :(
+    }
+
+
+    cout << endl;
 }
 
 		/*  Graphs  */
-class Edge
+class Graph
 {
 	
+          
 };
-
-
 
 
 
 int main()
 {
-    //std::cout << "Hello World!\n";
-
 
 			/*  TREES */
-    Node* Root = CreateTreeNode(1, nullptr);
-    Node* FirstChild = CreateTreeNode(2, Root);
-    Node* SecondChild = CreateTreeNode(3, Root);
-    Node* ThreeChild = CreateTreeNode(42, Root);
-    Node* FourthChild = CreateTreeNode(5, SecondChild);
-    Node* FifthChild = CreateTreeNode(6, SecondChild);
+    TreeNode* Root = CreateTreeNode(1, nullptr);
+    TreeNode* FirstChild = CreateTreeNode(2, Root);
+    TreeNode* SecondChild = CreateTreeNode(3, Root);
+    TreeNode* ThirdChild = CreateTreeNode(42, Root);
+    TreeNode* FourthChild = CreateTreeNode(5, SecondChild);
+    TreeNode* FifthChild = CreateTreeNode(6, SecondChild);
 
-    isRoot(Root);
-    isRoot(FifthChild);
-    preorderTraversal(Root);
+    
+           /* Functions to use for Trees*/
+
+    //GetRoot(INSERTANODE);
+
+    //GetChildren(INSERTANODE);
+   
+    //GetParent(INSERTANODE);
+
+    //Size(INSERTANODE);
+
+    //Depth(INSERTANODE);
+
+    //isEmpety(INSERTANODE);
+
+    //isRoot(INSERTANODE);
+
+    //isLeaf(INSERTANODE)
+
+    //deleteNode(INSETANODE);
+
+    //preorderTraversal(INSERTANODE);
+
+         /* Functions to use for Graphs*/
+
+
 }
